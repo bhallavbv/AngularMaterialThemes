08591756505e6afd512da1196650fc42fe4bb8fa
Vaibhav Bhalla <vaibhav.bhalla@basware.com>
2020-06-17 23:01:03 +0530

    Material Theming files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,15 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { MatButtonModule } from "@angular/material/button";
+import { MaterialModule } from "./material-module";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule,
+    MaterialModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
